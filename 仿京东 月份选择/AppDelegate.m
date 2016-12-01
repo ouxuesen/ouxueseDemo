885a15028015a85beefa32a6c2f062ab52a3a6be
ouxuesen <ouxuesen@le.com>
2016-12-01 17:22:56 +0800

    sync

--- 仿京东 月份选择/AppDelegate.m
+++ 仿京东 月份选择/AppDelegate.m
@@ -20,7 +20,21 @@
    
     return YES;
 }
-
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    // Check the calling application Bundle ID
+    if ([sourceApplication isEqualToString:@"com.apple.mobilesafari"])
+    {
+        NSLog(@"Calling Application Bundle ID: %@", sourceApplication);
+        NSLog(@"URL scheme:%@", [url scheme]);
+        NSLog(@"URL query: %@", [url query]);
+        
+        return YES;
+    }
+    else
+        return NO;
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
