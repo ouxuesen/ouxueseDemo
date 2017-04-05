896462834c13358844a7f994e6ee2a6a45914fe3
ouxuesen <ouxuesen@le.com>
2017-04-05 10:00:27 +0800

    sync

--- OXSFunctionDemo/autolyout/AutolayoutViewController.m
+++ OXSFunctionDemo/autolyout/AutolayoutViewController.m
@@ -32,6 +32,7 @@
     NSMutableArray * tempArray = [NSMutableArray new];
     for (int i =0; i<4; i++) {
         UILabel * lable  = [[UILabel alloc]init];
+        lable.backgroundColor = [UIColor orangeColor];
         lable.translatesAutoresizingMaskIntoConstraints = NO;
         [continerView addSubview:lable];
         lable.textAlignment  = NSTextAlignmentCenter;
@@ -50,20 +51,65 @@
     NSLayoutConstraint * left_1 = [NSLayoutConstraint constraintWithItem:lable_1 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:continerView attribute:NSLayoutAttributeLeft multiplier:1 constant:0];
     [continerView addConstraint:left_1];
     NSLayoutConstraint * left_2 = [NSLayoutConstraint constraintWithItem:lable_2 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:lable_1 attribute:NSLayoutAttributeRight multiplier:1 constant:5];
-    [lable_2 addConstraint:left_2];
+    [continerView addConstraint:left_2];
     NSLayoutConstraint * left_3 = [NSLayoutConstraint constraintWithItem:lable_3 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:lable_2 attribute:NSLayoutAttributeRight multiplier:1 constant:5];
-    [lable_3 addConstraint:left_3];
+    [continerView addConstraint:left_3];
     NSLayoutConstraint * left_4 = [NSLayoutConstraint constraintWithItem:lable_4 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:lable_3 attribute:NSLayoutAttributeRight multiplier:1 constant:5];
-    [lable_4 addConstraint:left_4];
-    NSLayoutConstraint * right_1 = [NSLayoutConstraint constraintWithItem:lable_4 attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:lable_3 attribute:NSLayoutAttributeRight multiplier:1 constant:0];
+    [continerView addConstraint:left_4];
+    
+    NSLayoutConstraint * right_1 = [NSLayoutConstraint constraintWithItem:lable_4 attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:continerView attribute:NSLayoutAttributeRight multiplier:1 constant:0];
     [continerView addConstraint:right_1];
     
     NSLayoutConstraint * width_1 = [NSLayoutConstraint constraintWithItem:lable_2 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:lable_1 attribute:NSLayoutAttributeWidth multiplier:1 constant:0];
-    [lable_2 addConstraint:width_1];
+    [continerView addConstraint:width_1];
     NSLayoutConstraint * width_2 = [NSLayoutConstraint constraintWithItem:lable_3 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:lable_1 attribute:NSLayoutAttributeWidth multiplier:1 constant:0];
-    [lable_3 addConstraint:width_2];
+    [continerView addConstraint:width_2];
     NSLayoutConstraint * width_3 = [NSLayoutConstraint constraintWithItem:lable_4 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:lable_1 attribute:NSLayoutAttributeWidth multiplier:1 constant:0];
-    [lable_4 addConstraint:width_3];
+    [continerView addConstraint:width_3];
+   
+    //1.添加两个控件
+    UIView *blueView = [[UIView alloc] init];
+    blueView.backgroundColor = [UIColor blueColor];
+    
+    blueView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:blueView];
+    
+    UIView *redView = [[UIView alloc] init];
+    redView.backgroundColor = [UIColor redColor];
+    redView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:redView];
+    
+    //2.添加约束
+    //2.1水平方向的约束
+    NSString *hVFL = @"H:|-30-[blueView]-30-[redView(==blueView)]-30-|";
+    NSArray *hCons = [NSLayoutConstraint constraintsWithVisualFormat:hVFL options:NSLayoutFormatAlignAllBottom | NSLayoutFormatAlignAllTop metrics:nil views:@{@"blueView":blueView, @"redView":redView}];
+    [self.view addConstraints:hCons];
+    
+    //2.2垂直方向的约束
+    NSString *vVFL = @"V:[blueView(50)]-30-|";
+    NSArray *vCons = [NSLayoutConstraint constraintsWithVisualFormat:vVFL options:0 metrics:nil views:@{@"blueView":blueView}];
+    [self.view addConstraints:vCons];
+    
+    NSMutableDictionary * tempDict = [NSMutableDictionary new];
+    UILabel * lableTEmp = nil;
+    for (int i =0; i<4; i++) {
+        UILabel * lable  = [[UILabel alloc]init];
+        if (i== 0) {
+            lableTEmp = lable;
+        }
+        lable.backgroundColor = [UIColor orangeColor];
+        lable.translatesAutoresizingMaskIntoConstraints = NO;
+        [continerView addSubview:lable];
+        lable.textAlignment  = NSTextAlignmentCenter;
+        lable.text = @(i).stringValue;
+        [tempDict setValue:lable forKey:[NSString stringWithFormat:@"lable_%d",i+1]];
+        [self.view addSubview:lable];
+    }
+    NSArray*constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-5-[lable_1]-5-[lable_2(==lable_1)]-5-[lable_3(==lable_2)]-5-[lable_4(==lable_3)]-5-|" options:NSLayoutFormatAlignAllBottom | NSLayoutFormatAlignAllTop metrics:nil views:tempDict];
+    [self.view addConstraints:constraints];
+    NSArray*constraints_1= [NSLayoutConstraint constraintsWithVisualFormat:@"V:[lable_1]-100-|" options:0 metrics:nil views:@{@"lable_1":lableTEmp}];
+    
+    [self.view addConstraints:constraints_1];
 }
 
 - (void)didReceiveMemoryWarning {
