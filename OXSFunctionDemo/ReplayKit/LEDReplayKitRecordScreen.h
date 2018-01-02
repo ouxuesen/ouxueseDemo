2cb0d6d40ea5a8a0140d529327fa0e3276d006bb
ouxuesen <xuesen@lexislive.com.cn>
2018-01-02 15:22:08 +0800

    sync

---
+++ OXSFunctionDemo/ReplayKit/LEDReplayKitRecordScreen.h
@@ -0,0 +1,13 @@
+//
+//  LEDReplayKitRecordScreen.h
+//  OXSFunctionDemo
+//
+//  Created by 欧学森 on 13/12/2017.
+//  Copyright © 2017 ouxuesen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LEDReplayKitRecordScreen : NSObject
+
+@end
---
+++ OXSFunctionDemo/ReplayKit/LEDReplayKitRecordScreen.m
@@ -0,0 +1,13 @@
+//
+//  LEDReplayKitRecordScreen.m
+//  OXSFunctionDemo
+//
+//  Created by 欧学森 on 13/12/2017.
+//  Copyright © 2017 ouxuesen. All rights reserved.
+//
+
+#import "LEDReplayKitRecordScreen.h"
+
+@implementation LEDReplayKitRecordScreen
+
+@end
